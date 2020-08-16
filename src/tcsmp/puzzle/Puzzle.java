3578640f2a5af1892d608795158203541cc4c79c
Mortada Ghanem <mghanem@cedaritservices.com>
2020-08-16 16:35:36 +0300

    Bug fix

--- src/tcsmp/puzzle/Puzzle.java
+++ src/tcsmp/puzzle/Puzzle.java
@@ -3,98 +3,96 @@ package tcsmp.puzzle;
 import java.util.Random;
 
 public class Puzzle {
-	
-	public char []bytes;
-	public static String hash;
-	
-	//server
+
+	public char[] bytes;
+	public String hash;
+
+	// server
 	public Puzzle() {
 		bytes = new char[36];
-		for(int i=0;i<36;i++) {
+		for (int i = 0; i < 36; i++) {
 			bytes[i] = '0';
 		}
 	}
-	
-	//client
-	public Puzzle(char[]c) {
+
+	// client
+	public Puzzle(char[] c) {
 		bytes = new char[36];
-		for(int i=0;i<36;i++) {
-			bytes[i] = (char)c[i];
+		for (int i = 0; i < 36; i++) {
+			bytes[i] = (char) c[i];
 		}
 	}
-	
-	
-	//server
+
+	// server
 	public void generate() {
-		int n = 2,s = 12;
+		int n = 2, s = 12;
 		Random r = new Random();
-		while(s < 33) {
+		while (s < 33) {
 			char c = (char) r.nextInt(26);
-			c+='A';
+			c += 'A';
 			bytes[n] = c;
 			bytes[s] = c;
-			n+=4;
-			s+=4;
+			n += 4;
+			s += 4;
 		}
-		int e = 1,w = 7;
-		while(w < 36) {
+		int e = 1, w = 7;
+		while (w < 36) {
 			char c = (char) r.nextInt(26);
-			c+='A';
+			c += 'A';
 			bytes[e] = c;
 			bytes[w] = c;
-			e+=4;
-			w+=4;
+			e += 4;
+			w += 4;
 		}
-		for(int i=0;i<36;i++) {
-			if(bytes[i] == '0') {
+		for (int i = 0; i < 36; i++) {
+			if (bytes[i] == '0') {
 				bytes[i] = (char) (r.nextInt(26) + 'A');
 			}
 		}
 		hash = toString();
 	}
 
-	//both
+	// both
 	public char charAt(int i) {
 		return bytes[i];
 	}
-	
-	
-	//server
+
+	// server
 	public void mix() {
 		Random r = new Random();
-		for(int i=0;i<9;i++) {
+		for (int i = 0; i < 9; i++) {
 			int times = r.nextInt(4);
-			for(int j=0;j<times;j++) {				
+			for (int j = 0; j < times; j++) {
 				move(i);
 			}
 		}
-		
-	}
-	
-	//both
-	public void move(int i) {
-		int pos = i*4;
-		char n = charAt(pos);
-		char e = charAt(pos +1);
-		char s = charAt(pos +2);
-		char w = charAt(pos +3);
-		bytes[pos] = w;
-		bytes[pos+1] = n;
-		bytes[pos +2] = e;
-		bytes[pos+3] = s;
+
 	}
 
-	//server
+	// both
+	public void move(int i) {
+		int pos = i * 4;
+		char n = charAt(pos);
+		char e = charAt(pos + 1);
+		char s = charAt(pos + 2);
+		char w = charAt(pos + 3);
+		bytes[pos] = w;
+		bytes[pos + 1] = n;
+		bytes[pos + 2] = e;
+		bytes[pos + 3] = s;
+	}
+
+	// server
 	public boolean verify(String answer) {
 		return hash.equals(answer);
 	}
-	
-	//both
+
+	// both
 	@Override
 	public String toString() {
 		String s = "";
-		for(int i=0;i<36;i++) {
-			s+=bytes[i];
+		for (int i = 0; i < 36; i++) {
+			s += bytes[i];
 		}
 		return s;
 	}
--- src/tcsmp/utils/DataUtils.java
+++ src/tcsmp/utils/DataUtils.java
@@ -7,11 +7,13 @@ import java.util.Random;
 public class DataUtils {
 
 	public static final String DOMAIN_NAME_PATTERN = "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}$";
-	public static HashMap<String, HostPort> servers = new HashMap<String, HostPort>();
-	private static final String zeroTo255 = "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])";
+	public static final String zeroTo255 = "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])";
 	public static final String IP_PATTERN = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
 	public static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+	
+	public static HashMap<String, HostPort> servers = new HashMap<String, HostPort>();
 	public static ArrayList<Email> HelloEmail = new ArrayList<Email>();
+	
 	static {
 		servers.put("gmail.com", new HostPort("localhost", 1234));
 		servers.put("hotmail.com", new HostPort("localhost", 1235));
